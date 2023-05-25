ae4ae288c4cc1c2f445613223322e746698059d9
Christian Garcia <bones192nd@gmail.com>
2023-05-25 11:52:07 -0400

    lab submission

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
-# your code goes here!
+import time
+
+
+def countdown(val):
+    while val > 0:
+        print(f"{val} SECOND(S)!")
+        val -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(val):
+    while val > 0:
+        print(f"{val} SECOND(S)!")
+        val -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+    
